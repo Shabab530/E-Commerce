55930b5bcc89265a94046e946741d40e02c88207
Mehedi Akram Shabab <112091272+Shabab530@users.noreply.github.com>
2025-09-01 14:42:54 +0600

    Update about.php

--- about.php
+++ about.php
@@ -79,7 +79,7 @@ if(isset($_SESSION['user_id'])){
             <i class="fas fa-star"></i>
             <i class="fas fa-star-half-alt"></i>
          </div>
-         <h3>john deo</h3>
+         <h3>Mehedi Akram Shabab</h3>
       </div>
 
       <div class="swiper-slide slide">
@@ -92,7 +92,7 @@ if(isset($_SESSION['user_id'])){
             <i class="fas fa-star"></i>
             <i class="fas fa-star-half-alt"></i>
          </div>
-         <h3>john deo</h3>
+         <h3>Jemima Rahman Mim</h3>
       </div>
 
       <div class="swiper-slide slide">
@@ -105,7 +105,7 @@ if(isset($_SESSION['user_id'])){
             <i class="fas fa-star"></i>
             <i class="fas fa-star-half-alt"></i>
          </div>
-         <h3>john deo</h3>
+         <h3>Kim Hung</h3>
       </div>
 
    </div>
